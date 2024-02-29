75e8224d7e7310332667d3048adfa4c2e1f38ea6
christinasamson <156719832+christinasamson@users.noreply.github.com>
2024-02-29 17:05:54 +0200

    Updated index.html

---
+++ index.js
@@ -0,0 +1,6 @@
+// show menu when clicked on mobile view
+document
+.getElementById("mobile-menu-toggle")
+.addEventListener("click", function() {
+    document.getElementById("mobile-menu").classList.toggle("hidden");
+})
